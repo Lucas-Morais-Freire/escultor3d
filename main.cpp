e45000bcc8825f9d0a022733be39ed1a1c7a24ab
Lucas Morais Freire <83522974+Lucas-Morais-Freire@users.noreply.github.com>
2022-05-27 10:25:01 -0300

    main v22

--- main.cpp
+++ main.cpp
@@ -55,90 +55,27 @@ void cutCylinder(Sculptor* S, float xa, float ya, float za, float xb, float yb, 
     }
 }
 
-void foot(Sculptor* S, int x, int y, int z) {
+void corpo(Sculptor* S, int x, int y, int z) {
     (*S).setColor(232,221,187);
-    putCylinder(S, 14 + x, 7.5 + y, 12 + z, 14 + x, 8.5 + y, 12 + z, 14); // coloca disco (raio 14)
-    cutCylinder(S, 14 + x, 7.5 + y, 12 + z, 14 + x, 8.5 + y, 12 + z, 13); // tira disco (raio 13)
-    (*S).putBox(7 + x, 21 + x, 14 + y, 16 + y, 7 + z, 17 + z); // suporte das 3 pernas
-    (*S).putBox(11 + x, 17 + x, 17 + y, 17 + y, 7 + z, 17 + z); // detalhe do suporte
-    (*S).cutBox(13 + x, 15 + x, 15 + y, 16 + y, 7 + z, 17 + z); // detalhe do suporte
-    (*S).cutBox(14 + x, 14 + x, 14 + y, 17 + y, 7 + z, 17 + z); // detalhe do suporte
-    putCylinder(S, 6 + x, 14 + y, 12 + z, 6 + x, 16 + y, 12 + z, 5); // parte arredondada do suporte
-    putCylinder(S, 22 + x, 14 + y, 12 + z, 22 + x, 16 + y, 12 + z, 5); // parte arredondada do suporte
-    (*S).putBox(9 + x, 19 + x, 19 + y, 23 + y, 7 + z, 17 + z); // caixa superior
-
-    (*S).setColor(93,93,93);
-    putCylinder(S, 0 + x, 4 + y, 12 + z, 10 + x, 8 + y, 12 + z, 1.5); // suporte dianteiro
-    putCylinder(S, 27 + x, 1 + y, 12 + z, 18 + x, 8 + y, 12 + z, 1.5); //suporte traseiro
-    putCylinder(S, 14 + x, 11 + y, 12 + z, 14 + x, 43 + y, 12 + z, 4); // perna principal
-    putCylinder(S, 5 + x, 17 + y, 12 + z, 5 + x, 25 + y, 12 + z, 2); // perna dianteira inf
-    putCylinder(S, 5 + x, 26 + y, 12 + z, 5 + x, 43 + y, 12 + z, 3); // perna dianteira sup
-    putCylinder(S, 23 + x, 17 + y, 12 + z, 23 + x, 25 + y, 12 + z, 2); // perna traseira inf
-    putCylinder(S, 23 + x, 26 + y, 12 + z, 23 + x, 43 + y, 12 + z, 3); // perna traseira sup
-
-    (*S).setColor(232,221,187);
-    (*S).putBox(1 + x, 17 + x, y, y, 1 + z, 23 + z); //base eixo z inf
-    (*S).putBox(x, 18 + x, 1 + y, 1 + y, z, 24 + z); //base eixo z sup
-    (*S).putBox(18 + x, 30 + x, y, 1 + y, 6 + z, 18 + z); // base eixo x
-    (*S).cutBox(30 + x, 30 + x, 1 + y, 1 + y, 6 + z, 18 + z); // detalhe base eixo x
-    (*S).putBox(x, 19 + x, 2 + y, 3 + y, 6 + z, 18 + z); // tampa da base
-    (*S).cutBox(17 + x, 19 + x, 3 + y, 3 + y, 6 + z, 18 + z); // detalhe tampa da base
-    putCylinder(S, 14 + x, 2 + y, 12 + z, 14 + x, 9 + y, 12 + z, 5); // tronco
-    putCylinder(S, 14 + x, 9.5 + y, 12 + z, 14 + x, 10.5 + y, 12 + z, 13); // disco maior
-    (*S).putBox(-1 + x, 1 + x, 2 + y, 4 + y, 6 + z, 18 + z); // "rabinho" dos pés
-    (*S).putBox(-2 + x, x, 3 + y, 5 + y, 6 + z, 18 + z); //
-    (*S).putBox(-3 + x, -2 + x, 5 + y, 6 + y, 6 + z, 18 + z); //
-}
-
-void RF_FootHolder(Sculptor* S, int x, int y, int z) {
-    (*S).setColor(232,221,187);
-    (*S).putBox(x, 77 + x, y, 10 + y, z, 58 + z);
-    (*S).putBox(34 + x, 67 + x, -3 + y, -1 + y, 30 + z, 30 + z);
-    (*S).putBox(34 + x, 67 + x, -6 + y, -4 + y, 31 + z, 31 + z);
-    (*S).putBox(34 + x, 67 + x, -9 + y, -7 + y, 32 + z, 32 + z);
-    (*S).putBox(34 + x, 67 + x, -3 + y, -1 + y, 2 + z, 2 + z);
-    (*S).putBox(34 + x, 67 + x, -6 + y, -4 + y, 1 + z, 1 + z);
-    (*S).putBox(34 + x, 67 + x, -9 + y, -7 + y, z, z);
-
-    (*S).setColor(0,0,0);
-    for (int i = 0; i <= 77; i++) {
-        (*S).cutBox(i + x, i + x, y, 10 + y, (3655 - i)/63 + 1 + z, 59 + z);
-    }
-    for (int i = 64; i <= 77; i++) {
-        (*S).cutBox(i + x, i + x, y, 10 + y, 121 - i + z, 57 + z);
-    }
-    for (int i = 0; i <= 5; i++) {
-        (*S).cutBox(i + x, i + x, y, 10 + y, z, (-29*i + 290)/5 - 2 + z);
-    }
-    for (int i = 6; i <= 16; i++) {
-        (*S).cutBox(i + x, i + x, y, 10 + y, z, (-26*i + 432)/10 - 1 + z);
-    }
-    (*S).cutBox(34 + x, 67 + x, y, 5 + y, 2 + z, 30 + z);
-
-}
-
-void RF_foot(Sculptor *S, int x, int y, int z) {
-    RF_FootHolder(S, x, y + 48, z);
-    foot(S, x + 38, y + 10, z + 4);
-}
-
-int main() {
-    Sculptor falcon(300,300,200);
-
-    falcon.setColor(232,221,187);
-    falcon.putEllipsoid(75, 15, 75, 75, 15, 75);
-    falcon.cutEllipsoid(75, 15, 75, 73, 13, 73);
+    (*S).putEllipsoid(75 + x, 15 + y, 75 + z, 75, 15, 75);
+    (*S).cutEllipsoid(75 + x, 15 + y, 75 + z, 73, 13, 73);
     for (int i = 0; i <= 150; i++) {
         for (int j = 0; j <= 30; j++) {
             for (int k = 0; k <= 150; k++) {
                 if ((i - 75)*(i - 75) + (k - 75)*(k - 75) >= 73*73) {
-                    falcon.cutVoxel(i, j, k);
+                    (*S).cutVoxel(i + x, j + y, k + z);
                 }
             }
         }
     }
-    putCylinder(&falcon, 75, 12, 75, 75, 18, 75, 73);
-    cutCylinder(&falcon, 75, 12, 75, 75, 18, 75, 71);
+    putCylinder(S, 75 + x, 12 + y, 75 + z, 75 + x, 18 + y, 75 + z, 73);
+    cutCylinder(S, 75 + x, 12 + y, 75 + z, 75 + x, 18 + y, 75 + z, 71);
+}
+
+int main() {
+    Sculptor falcon(400,400,250);
+
+    corpo(&falcon, 0, 10, 0);
 
     falcon.writeOFF("falcon.off");
 
