4f04fa457fe0f0262c81072bc91acebd79419850
Lucas Morais Freire <83522974+Lucas-Morais-Freire@users.noreply.github.com>
2022-06-01 05:40:05 -0300

    Add files via upload

---
+++ search/all_5.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['sculptor_0',['Sculptor',['../class_sculptor.html',1,'']]],
+  ['setcolor_1',['setColor',['../class_sculptor.html#a5723e671e073ac310b6f385a7fdc9f9f',1,'Sculptor']]]
+];
---
+++ search/functions_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['setcolor_0',['setColor',['../class_sculptor.html#a5723e671e073ac310b6f385a7fdc9f9f',1,'Sculptor']]]
+];
